a1cfff278ac6281c437bfd2855fd056d1100daa2
Ruddoll <moyo.adewusi@gmail.com>
2023-04-16 21:12:32 +1000

    added dark mode

--- js/index.js
+++ js/index.js
@@ -3,3 +3,8 @@ const navToggle = document.querySelector('.nav-toggle');
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
